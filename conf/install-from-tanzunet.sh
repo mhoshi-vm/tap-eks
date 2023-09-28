7852b2e3f8a05cf3bac2c3e9ab03bef7465ba54b
machih <machih@vmware.com>
2023-09-29 01:09:38 +0900

    Add a way to allow only from specific cidr block

--- conf/install-from-tanzunet.sh
+++ conf/install-from-tanzunet.sh
@@ -13,14 +13,4 @@ if [ "${TANZUNET_TOKEN}" != "" ]; then
        code-server --install-extension ${vsix}
    done
    rm -f *.vsix
- 
-   pivnet download-product-files --product-slug='tanzu-application-platform' --release-version=${TAP_VERSION} --glob='tanzu-framework-linux-amd64-*.tar'
- 
-   export TANZU_CLI_NO_INIT=true
-   tar xvf tanzu-framework-*.tar
-   mkdir -p $HOME/.local/bin
-   install cli/core/*/tanzu-core-*_amd64 $HOME/.local/bin/tanzu
-   $HOME/.local/bin/tanzu plugin install --local cli all
-   rm -f tanzu-framework-*.tar
-   rm -rf cli
 fi
